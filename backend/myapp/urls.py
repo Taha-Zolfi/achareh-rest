c243ab21de0b81ba3ff93f2505ab4ba00512ae10
Taha-Zolfi <115284592+Taha-Zolfi@users.noreply.github.com>
2024-08-14 09:40:54 +0330

    Update urls.py

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/' , views.get_student , name='get_student'),
-    path('users/create/' , views.create_student , name='create-student'),
+
     path('register/', views.register, name='user_registration'),
     path('login/', views.login, name='user_login'),
     path('home/', views.home, name='home'),
